834dc7d9e920fa24b729ea024fa5071e63ba4a3f
Hey Limi <hyelim3316@gmail.com>
2019-02-26 23:41:37 -0500

    css update

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+var computerChoice = ["t","o","t","o","r","o"];
+
+var win
+
+
+
+
+//first code
+
+var onclick=
+
+document.querySelector('body').style.backgroundColor='powderblue';
+
+$(document).ready(function() {
+
+
+$("#start-button").on("click", function() {
+   ;
+});
+
+});
+
+var userGuess = ["t","o","t","o","r","o"]
+
+document.onkeyup = function(event) {
+
+ 
+
+
+
+}
+
